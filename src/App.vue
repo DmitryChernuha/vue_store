b70ebb9f3f28b2a10612d4ee686606dd2336539a
User <chernuhadmitry@gmail.com>
2023-12-16 10:02:05 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup></script>
+
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <header>
+      <div>
+        <img src="/logo.png" alt="Logo" />
+      </div>
+      <div>
+        <h2>Vue Snickers</h2>
+        <p>Store of best cross</p>
+      </div>
+      <ul>
+        <li>
+          <img src="/cart.svg" alt="" />
+          <bold>1205</bold>
+        </li>
+        <li>
+          <img src="/heart.svg" alt="" />
+          <bold>Favorite</bold>
+        </li>
+
+        <li>
+          <img src="/profile.svg" alt="" />
+          <bold>Profile</bold>
+        </li>
+      </ul>
+    </header>
+  </div>
+</template>
